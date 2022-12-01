8cb0f23ed70914c0dc85fdb7fe849389672380d3
Jorge Emiliano Maldonado <jorgeemilianom@gmail.com>
2022-11-30 21:16:12 -0300

    Correcciones en el JS de mierda

--- pages/stock.php
+++ pages/stock.php
@@ -81,11 +81,14 @@ require('../config/core.php');
 
 
                     <form method="POST" autocomplete="OFF">
-                        <div class="autocomplete">
-                            <input id="myInput" type="text" name="telefono" placeholder="Seleccione Telefono" autocomplete="OFF">
-                        </div>
 
+                        <input list='select_phone' name="telefono" placeholder="Seleccione Telefono" autocomplete="OFF">
 
+                        <datalist id="select_phone">
+                            <?php foreach (Productos::getTelefonos() as $telefono) : ?>
+                                <option value="<?= $telefono['modelo'] . " " . $telefono['color'] . " - " . $telefono['capacidad'] . "GB |" . $telefono['id']  ?>"></option>
+                            <?php endforeach; ?>
+                        </datalist>
                         <div class="d-flex mt-3" style="gap: 20px">
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="productoSellado" id="flexRadioDefault1" value="1">
@@ -115,19 +118,19 @@ require('../config/core.php');
                         </div>
                         <div class="mb-3">
                             <label for="precio_venta" class="form-label">Precio de venta</label>
-                            <input type="number" step="0.01" class="form-control" id="precio_venta" name="precio_venta" >
+                            <input type="number" step="0.01" class="form-control" id="precio_venta" name="precio_venta">
                         </div>
                         <div class="mb-3">
                             <label for="costo" class="form-label">Costo</label>
-                            <input type="number" step="0.01" class="form-control" id="costo" name="costo" >
+                            <input type="number" step="0.01" class="form-control" id="costo" name="costo">
                         </div>
                         <div class="mb-3">
                             <label for="fecha_ingreso" class="form-label">Fecha de ingreso</label>
-                            <input type="date" class="form-control" id="fecha_ingreso" name="fecha_ingreso" >
+                            <input type="date" class="form-control" id="fecha_ingreso" name="fecha_ingreso">
                         </div>
                         <div class="mb-3">
                             <label for="bateria" class="form-label">Bateria</label>
-                            <input type="number" step="0.1" class="form-control" id="bateria" name="bateria" >
+                            <input type="number" step="0.1" min="0" max="100" class="form-control" id="bateria" name="bateria">
                         </div>
 
                         <input type="hidden" name="carga_stock_celular" value="1">
@@ -146,120 +149,7 @@ require('../config/core.php');
 </body>
 
 <script>
-    
-    var countries = [
-        <?php foreach (Productos::getTelefonos() as $telefono) : ?>
-            "<?= $telefono['modelo'] . " " . $telefono['color'] . " - " . $telefono['capacidad'] . "GB |" . $telefono['id']  ?>",
-        <?php endforeach; ?>
-    ];
-
-    function autocomplete(inp, arr) {
-        /*the autocomplete function takes two arguments,
-        the text field element and an array of possible autocompleted values:*/
-        var currentFocus;
-        /*execute a function when someone writes in the text field:*/
-        inp.addEventListener("input", function(e) {
-            var a, b, i, val = this.value;
-            /*close any already open lists of autocompleted values*/
-            closeAllLists();
-            if (!val) {
-                return false;
-            }
-            currentFocus = -1;
-            /*create a DIV element that will contain the items (values):*/
-            a = document.createElement("DIV");
-            a.setAttribute("id", this.id + "autocomplete-list");
-            a.setAttribute("class", "autocomplete-items");
-            /*append the DIV element as a child of the autocomplete container:*/
-            this.parentNode.appendChild(a);
-            /*for each item in the array...*/
-            for (i = 0; i < arr.length; i++) {
-                /*check if the item starts with the same letters as the text field value:*/
-                if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                    /*create a DIV element for each matching element:*/
-                    b = document.createElement("DIV");
-                    /*make the matching letters bold:*/
-                    b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-                    b.innerHTML += arr[i].substr(val.length);
-                    /*insert a input field that will hold the current array item's value:*/
-                    b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-                    /*execute a function when someone clicks on the item value (DIV element):*/
-                    b.addEventListener("click", function(e) {
-                        /*insert the value for the autocomplete text field:*/
-                        inp.value = this.getElementsByTagName("input")[0].value;
-                        /*close the list of autocompleted values,
-                        (or any other open lists of autocompleted values:*/
-                        closeAllLists();
-                    });
-                    a.appendChild(b);
-                }
-            }
-        });
-        /*execute a function presses a key on the keyboard:*/
-        inp.addEventListener("keydown", function(e) {
-            var x = document.getElementById(this.id + "autocomplete-list");
-            if (x) x = x.getElementsByTagName("div");
-            if (e.keyCode == 40) {
-                /*If the arrow DOWN key is pressed,
-                increase the currentFocus variable:*/
-                currentFocus++;
-                /*and and make the current item more visible:*/
-                addActive(x);
-            } else if (e.keyCode == 38) { //up
-                /*If the arrow UP key is pressed,
-                decrease the currentFocus variable:*/
-                currentFocus--;
-                /*and and make the current item more visible:*/
-                addActive(x);
-            } else if (e.keyCode == 13) {
-                /*If the ENTER key is pressed, prevent the form from being submitted,*/
-                e.preventDefault();
-                if (currentFocus > -1) {
-                    /*and simulate a click on the "active" item:*/
-                    if (x) x[currentFocus].click();
-                }
-            }
-        });
-
-        function addActive(x) {
-            /*a function to classify an item as "active":*/
-            if (!x) return false;
-            /*start by removing the "active" class on all items:*/
-            removeActive(x);
-            if (currentFocus >= x.length) currentFocus = 0;
-            if (currentFocus < 0) currentFocus = (x.length - 1);
-            /*add class "autocomplete-active":*/
-            x[currentFocus].classList.add("autocomplete-active");
-        }
-
-        function removeActive(x) {
-            /*a function to remove the "active" class from all autocomplete items:*/
-            for (var i = 0; i < x.length; i++) {
-                x[i].classList.remove("autocomplete-active");
-            }
-        }
-
-        function closeAllLists(elmnt) {
-            /*close all autocomplete lists in the document,
-            except the one passed as an argument:*/
-            var x = document.getElementsByClassName("autocomplete-items");
-            for (var i = 0; i < x.length; i++) {
-                if (elmnt != x[i] && elmnt != inp) {
-                    x[i].parentNode.removeChild(x[i]);
-                }
-            }
-        }
-        /*execute a function when someone clicks in the document:*/
-        document.addEventListener("click", function(e) {
-            // console.log($("#myInput").val());
-            closeAllLists(e.target);
-        });
-
-    }
-
-    autocomplete(document.getElementById("myInput"), countries);
-
-
+ 
     $('.boton_de_siguiente_en_venta_plan_canje_si').click(() => {
         $('.step1_venta').css('display', 'none');
         $('.step2_venta').css('display', 'block');
